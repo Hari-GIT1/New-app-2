8ba63c24f1e3904d85e9b4154d61b792e2726c52
Hari-GIT1 <nagellyhariharan@gmail.com>
2024-12-10 16:47:30 +0530

    Login reactive Form Implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  public loginForm:FormGroup =new FormGroup({
+    email:new FormControl("",[Validators.required,Validators.email]),
+    password: new FormControl("",[Validators.required,Validators.minLength(4)])
+  })
+
+submit(){
+  console.log(this.loginForm.value)
+}
 
 }
