beae53ea27ceb569374c2d3d8baeccbdc4bf8c4b
Hari-GIT1 <nagellyhariharan@gmail.com>
2024-12-10 17:02:25 +0530

    Login Service Implemented

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +10,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
+  constructor(private _loginServices:LoginService, private _router:Router){}
+
   public loginForm:FormGroup =new FormGroup({
     email:new FormControl("",[Validators.required,Validators.email]),
-    password: new FormControl("",[Validators.required,Validators.minLength(4)])
+    password: new FormControl("",[Validators.required,Validators.minLength(4),Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')])
   })
 
 submit(){
-  console.log(this.loginForm.value)
+  this._loginServices.getLogin(this.loginForm.value).subscribe(
+    (data:any)=>{
+      alert("Login Success .....")
+      this._router.navigateByUrl("/dashboard")
+    },
+    (err:any)=>{alert("Login Failed !!!")}
+  )
 }
 
 }
