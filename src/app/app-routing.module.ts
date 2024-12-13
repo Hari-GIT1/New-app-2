2747d0e099f491c45161b80257354eae062f3b87
Hari-GIT1 <nagellyhariharan@gmail.com>
2024-12-13 14:40:25 +0530

    dashboard implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
+import { createComponent, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { PagenotfoundComponent } from './components/pagenotfound/pagenotfound.component';
 import { AuthenticationGuard } from './guards/authentication.guard';
+import { CreateStudentsComponent } from './components/create-students/create-students.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
 
 const routes: Routes = [
   {path:"login",component:LoginComponent},
-  {path:"dashboard",component:DashboardComponent, canActivate:[AuthenticationGuard]},
+  {path:"dashboard",component:DashboardComponent, canActivate:[AuthenticationGuard],children:[
+    {path:'create',component:CreateStudentsComponent},
+    {path:'students',component:StudentDetailsComponent}
+  ]},
   {path:"",component:LoginComponent},
-  {path:"**",component:PagenotfoundComponent}
+  {path:"**",component:PagenotfoundComponent},
 ];
 
 @NgModule({
