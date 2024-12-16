9bc9b687e0f24394472f7980e6882b8a429940d3
Hari-GIT1 <nagellyhariharan@gmail.com>
2024-12-16 13:32:43 +0530

    address part completed in form

--- src/app/components/create-students/create-students.component.ts
+++ src/app/components/create-students/create-students.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-students',
@@ -9,13 +9,65 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class CreateStudentsComponent {
 
   studentForm:FormGroup = new FormGroup({
-    name:new FormControl(),
-    gender:new FormControl(),
-    mobile:new FormControl(),
-    email:new FormControl(),
-    batch:new FormControl()
+    name:new FormControl('',[Validators.required]),
+    gender:new FormControl('',[Validators.required]),
+    mobile:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required]),
+    batch:new FormControl('',[Validators.required]),
+
+    address:new FormGroup({
+
+      city:new FormControl('',[Validators.required]),
+      mandal:new FormControl('',[Validators.required]),
+      district:new FormControl('',[Validators.required]),
+      state:new FormControl('',[Validators.required]),
+      pincode:new FormControl('',[Validators.required])
+
+    }),
+    education:new FormArray([]),
+    companyDetails:new FormGroup({
+      name:new FormControl('',[Validators.required]),
+      location:new FormControl('',[Validators.required]),
+      package:new FormControl('',[Validators.required]),
+      offerDate:new FormControl('',[Validators.required]),
+    }),
+    sourceType:new FormControl('',[Validators.required]),
+
   })
 
+  constructor(){
+    this.studentForm.get('sourceType')?.valueChanges.subscribe(
+      (data:any)=>{
+        if(data=='Direct'){
+          this.studentForm.addControl('sourceFrom', new FormControl())
+          this.studentForm.removeControl('referalName')
+        }
+        else{
+          this.studentForm.addControl('referalName',new FormControl())
+          this.studentForm.removeControl('SourceFrom')
+        }
+      }
+    )
+
+
+  }
+
+  get educationFormArray(){
+    return this.studentForm.get('education') as FormArray;
+  }
+  addEdu(){
+    this.educationFormArray.push(
+      new FormGroup({
+        qualification:new FormControl('',[Validators.required]),
+        year: new FormControl('',[Validators.required]),
+        percentage:new FormControl('',[Validators.required]),
+      })
+    )
+
+  }
+  eduDel(i:number){
+    this.educationFormArray.removeAt(i)
+  }
 
   submit(){
     console.log(this.studentForm.value)
